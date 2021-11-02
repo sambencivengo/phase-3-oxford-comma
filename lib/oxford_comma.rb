ed05a731b4e8396be15c7670a1df35c78d915781
Sam Bencivengo <sambencivengo@gmail.com>
2021-11-02 11:49:59 -0400

    commit tree c1bed7885ab9cd1d48f757f09220bbbf7c923905

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+  if array.length == 1
+    array.join
+    elsif array.length == 2
+      array.join(" and ")
+      elsif array.length >= 3
+        # remove last element, store as variable
+        last_el = array.pop
+        # interpolate last element to include ", and ", store as variable
+        and_el = ", and #{last_el}"
+        # join mutated array with commas
+        joined = array.join(", ")
+        # concat new strings and return result
+        oxford = joined + and_el
+        oxford
+    end
 end
+
+
+
+puts oxford_comma ["fiddleheads", "okra", "kohlrabi", "something", "something else"]
